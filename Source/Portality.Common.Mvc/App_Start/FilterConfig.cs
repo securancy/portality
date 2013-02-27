66e366ac6e951c37087ec42c8f1bc7a9fb47e96a
Juliën Hanssens <github@hanssens.com>
2013-02-27 11:40:15 +0100

    Added empty boilerplate projects for .NET 4.5

---
+++ Source/Portality.Common.Mvc/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Portality.Common.Mvc
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
